2ef5bae0047bf99246c78e6855e4200465215726
Nate Bucher <n8bizall@yahoo.com>
2018-03-16 09:39:00 -0400

    added student

---
+++ student.class.ts
@@ -0,0 +1,11 @@
+export class Student {
+    Id: number;
+    FName: string;
+    Lname:string;
+    GPA: number;
+    Major: string;
+    Year: string;
+    constructor(parameters) {
+        
+    }
+}
